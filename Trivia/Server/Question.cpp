b1359066ce73393c7ffa9f53e4a5fd6c85442254
Eyal Cherevatzki <eyal.3555@gmail.com>
2016-05-05 18:31:16 +0300

    Question should be ready
    Question should be ready

--- Trivia/Server/Question.cpp
+++ Trivia/Server/Question.cpp
@@ -1,8 +1,19 @@
 #include "Question.h"
+#include <vector>
+#include <algorithm>
 
 
-Question::Question()
+std::default_random_engine Question::_engine = std::default_random_engine{};
+
+Question::Question(int id, std::string question, std::string correctAnswer, std::string answer2, std::string answer3, std::string answer4) : _id(id), _question(question), _correctAnswer(correctAnswer), _answer2(answer2), _answer3(answer3), _answer4(answer4)
 {
+	std::vector<std::string> answers({ _correctAnswer, _answer2, _answer3, _answer4 });
+	shuffle(answers.begin(), answers.end(), _engine);
+	
+	_correctIndex = std::find(answers.begin(), answers.end(), _correctAnswer) - answers.begin();
+	
+	for (int i = 0; i < answers.size(); i++)
+		_answers[i] = answers[i];
 }
 
 
