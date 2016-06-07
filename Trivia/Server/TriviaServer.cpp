f9aa519d7f429c8043382352b40dbcd19918aa4f
Eyal Cherevatzki <eyal.3555@gmail.com>
2016-06-07 19:53:24 +0300

    Sync 7-Jun-2016
    Just synchronizing

--- Trivia/Server/TriviaServer.cpp
+++ Trivia/Server/TriviaServer.cpp
@@ -1,10 +1,22 @@
 #include "TriviaServer.h"
+#include <sstream>
 
 
 TriviaServer::TriviaServer()
 {
-}
+	_db = Database();
 
+	WSADATA wsaData;
+	int iResult;
+
+	if ((iResult = WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0)
+	{
+		std::stringstream ex;
+		ex << "WSAStartup failed: " << iResult << "\n";
+
+		throw std::exception(ex.str().c_str());
+	}
+}
 
 TriviaServer::~TriviaServer()
 {
--- Trivia/Server/TriviaServer.h
+++ Trivia/Server/TriviaServer.h
@@ -1,8 +1,30 @@
 #pragma once
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <map>
+#include <mutex>
+#include <queue>
+
+#include "User.h"
+#include "Database.h"
+#include "Room.h"
+#include "ReceivedMessage.h"
+
 class TriviaServer
 {
 public:
 	TriviaServer();
 	~TriviaServer();
-};
 
+private:
+	SOCKET _socket;
+	std::map<SOCKET, User*> _connectedUsers;
+	Database _db;
+	std::map<int, Room*> _roomsList;
+
+	std::mutex _mtxReceivedMessages;
+	std::queue<ReceivedMessage*> _queRcvMessages;
+
+	static int _roomIdSequence;
+};
